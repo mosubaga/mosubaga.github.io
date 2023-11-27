a9b59ffc3afe8898e06e8232fe11870539bfbc75
NOBUHIKO HAYASHI <mosubaga@icloud.com>
2023-11-26 21:07:14 -0500

    Add rotk stuff

--- js/script.js
+++ js/script.js
@@ -21,10 +21,10 @@ function addZero(n) {
   return (parseInt(n,10) < 10 ? "0" : '') + n
 }
 
-function getdata()
+function getrotktwelve()
 {   
     const rotk = "js/rotk12.json";
-    let sResult = "";
+    let sResult = "<table class='table'><tr><th>Name</th><th>Other</th><th>BAT</th><th>STR</th><th>INT</th><th>POL</th></tr>";
     fetch(rotk)
         .then((response) => {
             return response.json();
@@ -35,6 +35,40 @@ function getdata()
             myChars.forEach(async (char) => {
                 sResult = sResult + `<tr><td>${char.cNAME}</td><td>${char.cONAME}</td><td>${char.cBAT}</td><td>${char.cWAR}</td><td>${char.cINT}</td><td>${char.cPOL}</td></tr>`;
             });
-            document.getElementById("santwelve").innerHTML = sResult;
+            document.getElementById("genlist").innerHTML = sResult + "</table>";
         });
+}
+
+function getrotkeleven()
+{
+    const rotk = "js/rotk11.json";
+    let sResult = "<table class='table'><tr><th>Name</th><th>BAT</th><th>STR</th><th>INT</th><th>POL</th><th>CHM</th></tr>";
+    fetch(rotk)
+        .then((response) => {
+            return response.json();
+        })
+        .then((myJson) => {
+            // console.log(myJson["characters"]);
+            const myChars = myJson["characters"]
+            myChars.forEach(async (char) => {
+                sResult = sResult + `<tr><td>${char.cNAME}</td></td><td>${char.cBAT}</td><td>${char.cWAR}</td><td>${char.cINT}</td><td>${char.cPOL}</td><td>${char.cCHM}</td></tr>`;
+            });
+            document.getElementById("genlist").innerHTML = sResult + "</table>";
+        });
+}
+
+function dispdata()
+{
+    selectElement = document.querySelector('#version');
+    output = selectElement.value;
+
+    if (output === "rotk11") {
+        getrotkeleven();
+    }
+    else if (output === "rotk12"){
+        getrotktwelve();
+    }
+    else {
+        alert("No version selected");
+    }
 }
