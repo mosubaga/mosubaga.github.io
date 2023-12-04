5e02d730d318413b07c9451e1f651eb920f27539
mosubaga <42124945+mosubaga@users.noreply.github.com>
2023-12-03 20:15:54 -0500

    Fix button

--- js/script.js
+++ js/script.js
@@ -56,6 +56,7 @@ function getrotkeleven()
             });
             document.getElementById("genlist").innerHTML = sResult + "</table>";
             document.getElementById("filter").style.visibility="visible";
+            document.getElementById("reloadbutton").style.visibility="visible";
         });
 }
 
@@ -76,6 +77,7 @@ function getrotkfourteen()
             });
             document.getElementById("genlist").innerHTML = sResult + "</table>";
             document.getElementById("filter").style.visibility="visible";
+            document.getElementById("reloadbutton").style.visibility="visible";
         });
 }
 
@@ -95,6 +97,7 @@ function getrotknamco()
             });
             document.getElementById("genlist").innerHTML = sResult + "</table>";
             document.getElementById("filter").style.visibility="visible";
+            document.getElementById("reloadbutton").style.visibility="visible";
         });
 }
 
