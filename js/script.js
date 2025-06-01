b3cd1d4c4cac0af90d9f7f40a0b23ca4122687d3
mosubaga <42124945+mosubaga@users.noreply.github.com>
2025-05-31 20:54:11 -0400

    Added content

--- js/script.js
+++ js/script.js
@@ -163,9 +163,9 @@ function dispdata()
 function getbooks(publisher,topic,elementid)
 {
 
-    const ml_list = new Set(["Fundamentals of Data Engineering", "Hands-On Machine Learning with Scikit-Learn, Keras, and TensorFlow, 3rd Edition","Generative Deep Learning, 2nd Edition"]);
+    const ml_list = new Set(["Fundamentals of Data Engineering", "Hands-On Machine Learning with Scikit-Learn, Keras, and TensorFlow, 3rd Edition","Generative Deep Learning, 2nd Edition", "Hands-On Large Language Models"]);
     const react_list = new Set(["React Cookbook", "Learning React, 2nd Edition", "React: Up & Running, 2nd Edition"]);
-    const python_list = new Set(["Python Workout", "Python Concurrency with asyncio", "Deep Learning with Python, Second Edition", "Fluent Python, 2nd Edition", "Python in a Nutshell, 4th Edition", "Introducing Python, 2nd Edition","Robust Python" ]);
+    const python_list = new Set(["Python Workout", "Python Concurrency with asyncio", "Deep Learning with Python, Second Edition", "Fluent Python, 2nd Edition", "Python in a Nutshell, 4th Edition", "Introducing Python, 2nd Edition","Robust Python","Learning Python, 6th Edition" ]);
     const javascript_list = new Set(["JavaScript: The Definitive Guide, 7th Edition", "JavaScript Cookbook, 3rd Edition", "JavaScript Everywhere", "Learning JavaScript Design Patterns, 2nd Edition","Gatsby: The Definitive Guide"]);
     const go_list = new Set(["Go in Action", "Go in Practice", "100 Go Mistakes and How to Avoid Them"]);
 
@@ -218,4 +218,82 @@ function getbooks(publisher,topic,elementid)
         document.getElementById(elementid).innerHTML = sResult + "</table>";
 
     });
+}
+
+
+function getstrikeouts(){
+    const strikeout = "js/strikeouts.json";
+    let sResult = "<table class='table'><tr><th>Year</th><th>AL</th><th>NL</th><th>Total</th></tr>";
+    fetch(strikeout)
+        .then((response) => {
+            return response.json();
+        })
+        .then((myJson) => {
+            // console.log(myJson["players"]);
+            const myPlayers = myJson;
+            myPlayers.forEach(async (player) => {
+            sResult = sResult + `<tr><td>${player.YEAR}</td><td>${player.AL}</td><td>${player.NL}</td><td>${player.TOTAL}</td></tr>`;
+            });
+            document.getElementById("strikeoutlist").innerHTML = sResult + "</table>";
+        });
+
+}
+
+function plotPoints() {
+    const input = document.getElementById('inputArea').value.trim();
+    const lines = input.split('\n');
+    const points = [];
+    lines.forEach(line => {
+        const pair = line.split(',').map(Number);
+        if (pair.length === 2 && !isNaN(pair[0]) && !isNaN(pair[1])) {
+            points.push({x: pair[0], y: pair[1]});
+        }
+    });
+
+    // Set up SVG and dimensions
+    const svg = d3.select("#chart");
+    svg.selectAll("*").remove(); // Clear previous plot
+
+    const width = +svg.attr("width") - 60;
+    const height = +svg.attr("height") - 60;
+    const margin = {top: 30, right: 30, bottom: 30, left: 40};
+
+    // If no valid points, do nothing
+    if (points.length === 0) return;
+
+    // Find min and max
+    const xExtent = d3.extent(points, d => d.x);
+    const yExtent = d3.extent(points, d => d.y);
+
+    // Scales
+    const xScale = d3.scaleLinear()
+        .domain([xExtent[0], xExtent[1]])
+        .range([margin.left, width + margin.left]);
+
+    const yScale = d3.scaleLinear()
+        .domain([yExtent[0], yExtent[1]])
+        .range([height + margin.top, margin.top]);
+
+    // Axes
+    const xAxis = d3.axisBottom(xScale);
+    const yAxis = d3.axisLeft(yScale);
+
+    // Draw axes
+    svg.append("g")
+        .attr("transform", `translate(0,${height+margin.top})`)
+        .call(xAxis);
+
+    svg.append("g")
+        .attr("transform", `translate(${margin.left},0)`)
+        .call(yAxis);
+
+    // Draw points
+    svg.selectAll(".point")
+        .data(points)
+        .enter()
+        .append("circle")
+        .attr("class", "point")
+        .attr("cx", d => xScale(d.x))
+        .attr("cy", d => yScale(d.y))
+        .attr("r", 5);
 }
