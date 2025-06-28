6cfb93996d930c85233147c184bc68a8e4a964f4
mosubaga <42124945+mosubaga@users.noreply.github.com>
2025-06-28 19:03:45 -0400

    Added content

---
+++ fft/js/script.js
@@ -0,0 +1,26 @@
+
+
+function getjobs()
+{   
+    const sURL = "./js/jobs.json";
+    fetch(sURL)
+        .then((response) => {
+            return response.json();
+        })
+        .then((myJson) => {
+            let sResult = "<table border=\"1\"><tr><th>Job</th><th>Icon</th><th>Description</th><th>Qualifications</th></tr>"
+            console.log(myJson);
+            myJson.jobs.forEach(async (job) => {
+                console.log(job);
+                if ((job.job == "Dancer") || (job.job == "Bard")) {
+                    sResult = sResult + `<tr><td>${job.job}</td><td><img class="center" src="./img/${job.icon}" alt="${job.job}"></img></td><td>${job.description}</td><td>${job.qualifications}</td></tr>`;
+                }
+                else{
+                    sResult = sResult + `<tr><td>${job.job}</td><td><img src="./img/${job.ficon}" alt="${job.job}"></img><img src="./img/${job.micon}" alt="${job.job}"></img></td><td>${job.description}</td><td>${job.qualifications}</td></tr>`;
+                }
+            });
+            sResult = sResult + "</table>";
+            console.log(sResult);
+            document.getElementById("jobs").innerHTML = sResult;
+        });
+}
