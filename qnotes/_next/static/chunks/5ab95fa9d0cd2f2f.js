15c206b17228814d1b31e7a7a798223a480988e1
mosubaga <42124945+mosubaga@users.noreply.github.com>
2025-11-09 17:06:09 -0500

    Modify content

---
+++ qnotes/_next/static/chunks/5ab95fa9d0cd2f2f.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/995cf7cd0cb9d109.js",
+  "static/chunks/turbopack-4bbf59f4ed4bba98.js"
+])
---
+++ qnotes/_next/static/chunks/9c7d087fe2b43fdf.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/995cf7cd0cb9d109.js",
+  "static/chunks/turbopack-9323ff922d69e7c8.js"
+])
